29b6929650e99ee3e5a66df96a6639488218ad79
TacticalTarsier <tacticalalpaka@gmail.com>
2022-08-07 20:40:43 +0300

    Added simple script

---
+++ test_one.py
@@ -0,0 +1,7 @@
+import telegram
+
+# подключение к боту
+bot = telegram.Bot(token='5504212810:AAETMdBz7GHwrLXYmlt4lE03omb_080YifY')
+chat_id = bot.getUpdates()[-1]['message']['chat']['id']
+
+bot.sendMessage(text='Hi', chat_id=chat_id)
