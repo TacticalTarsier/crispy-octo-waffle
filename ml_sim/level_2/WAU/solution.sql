148d9a1ae738bf93141484b73132914b4911c0f7
TacticalTarsier <tacticalalpaka@gmail.com>
2023-02-01 22:56:13 +0300

    Path correction

--- ml_sim/level_2/WAU/solution.sql
+++
@@ -1,3 +0,0 @@
-select distinct toDate(timestamp) as day,
-       uniqExact(user_id) over(order by day range between 6 preceding and current row) as wau
-from default.churn_submits
--- ml_sim/level_2/elasticity_feature/solution.sql
+++
@@ -1,6 +0,0 @@
-select sku,
-       dates,
-       price,
-       count(sku) as qty
-from transactions
-group by 1, 2, 3
